9e0fcd073fcea656b1e9e689e605b3f274030f53
Ilin Ilchev <ilin.ilchev@gmail.com>
2026-03-12 12:20:58 +0200

    Add files via upload

---
+++ Mahjong/utils/constants.py
@@ -0,0 +1,14 @@
+# Game constants
+TILES_PER_SUIT = 9
+TILES_PER_TYPE = 4
+HAND_SIZE = 13
+WINNING_HAND_SIZE = 14
+WALL_SIZE = 136
+
+# Scoring constants
+BASE_POINTS = 1
+LIMIT_HANDS = {
+    'big_four_winds': 13,
+    'all_green': 13,
+    'nine_gates': 13
+}
